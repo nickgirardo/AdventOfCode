d9df8fd59c698f06955d38fd8bfcb8f3a2faf180
Nick Girardo <nickgirardo@gmail.com>
2023-12-01 19:26:23 -0500

    aoc2023: day one

---
+++ aoc2023/day1/app/Main.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import Data.Char (isDigit, digitToInt, toLower)
+import Control.Arrow ((&&&))
+import System.Environment (getArgs)
+import Data.Maybe (listToMaybe, catMaybes, mapMaybe)
+import Data.List (tails)
+
+main :: IO ()
+main = getArgs >>= \arg -> if listToMaybe arg == Just "--part-one" then go partOne else go partTwo
+    where
+      go :: (String -> IO ()) -> IO ()
+      go = (>>=) $ readFile "./input.txt"
+
+-- NOTE only makes sense if the input Ints are single digits
+joinDigits :: Int -> Int -> Int
+joinDigits a b = a*10 + b
+
+startsWith :: Eq a => [a] -> [a] -> Bool
+startsWith _ [] = True
+startsWith [] _ = False
+startsWith (x:xs) (y:ys) = x == y && startsWith xs ys
+
+partOne :: String -> IO ()
+partOne = print . processFile
+    where
+        processFile :: String -> Int
+        processFile = sum . map processLine . lines
+        processLine :: String -> Int
+        processLine = uncurry joinDigits . (&&&) head last . map digitToInt . filter isDigit
+
+partTwo :: String -> IO ()
+partTwo = print . processFile
+
+processFile :: String -> Int
+processFile = sum . map processLine . lines
+
+processLine :: String -> Int
+processLine = uncurry joinDigits . (&&&) head last . mapMaybe valueOfTail . tails . map toLower
+
+valueOfTail :: String -> Maybe Int
+valueOfTail = magicalLookup startsWith values
+  where
+        magicalLookup :: (a -> b -> Bool) -> [(b,c)] -> a -> Maybe c
+        magicalLookup _f [] _key = Nothing
+        magicalLookup f ((x,y):xys) key = if f key x then Just y else magicalLookup f xys key
+        values = [("1", 1),
+                  ("2", 2),
+                  ("3", 3),
+                  ("4", 4),
+                  ("5", 5),
+                  ("6", 6),
+                  ("7", 7),
+                  ("8", 8),
+                  ("9", 9),
+                  ("0", 0),
+                  ("one",   1),
+                  ("two",   2),
+                  ("three", 3),
+                  ("four",  4),
+                  ("five",  5),
+                  ("six",   6),
+                  ("seven", 7),
+                  ("eight", 8),
+                  ("nine",  9),
+                  ("zero",  0)]
